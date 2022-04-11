8a4325ebd4922ce693525b332146ed57286f5cd7
luis.er <luis.er@AN0101841066028.dinfor.uniceub>
2022-04-11 20:38:24 -0300

    adicionando variaveis

--- test/src/test/flamengo.java
+++ test/src/test/flamengo.java
@@ -1,8 +1,14 @@
 package test;
 
+import java.util.Scanner;
+
 public class flamengo {
 	public static void main(String[]args) {
-		
+	Scanner leitor = new Scanner(System.in);
+	int idade = 0; 
+	char nome = 0; 
+	int cpf = 0;
+	
 		
 		
 
